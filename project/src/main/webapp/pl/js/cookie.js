c13364c3dd022823c589f11f3fff28425262af6b
Sebastian Piecowski <seba.piecowski@gmail.com>
2018-06-30 14:19:31 +0200

    Final version

--- project/src/main/webapp/pl/js/cookie.js
+++ project/src/main/webapp/pl/js/cookie.js
@@ -20,5 +20,7 @@ function createBasic(userEmail, userPassword, role){
 function deleteAllCookies() {
  var c = document.cookie.split("; ");
  for (i in c) 
+  document.cookie ="";
+  document.cookie =/^[^=]+/.exec(c[i])[0]+"=;";    
   document.cookie =/^[^=]+/.exec(c[i])[0]+"=;expires=Thu, 01 Jan 1970 00:00:00 GMT";    
 }
